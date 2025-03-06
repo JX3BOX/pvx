8d9e63b6e93e89e0d827e06f1997c75da0644803
zhusha <yumg@whsmwy.com>
2025-03-06 14:08:33 +0800

    fix：style

--- src/views/body/mobile/List.vue
+++ src/views/body/mobile/List.vue
@@ -187,7 +187,7 @@ export default {
     @fontColor-dark: #fff;
     @fontColor-dark2: rgba(255, 255, 255, 0.8);
     @fontColor-dark3: rgba(255, 255, 255, 0.4);
-    padding: 12px 20px;
+    padding: 0 20px 12px 20px;
     .m-body-list_mobile__tabs {
         position: sticky;
         top: 0;
@@ -197,6 +197,7 @@ export default {
         align-items: center;
         height: 32px;
         background-color: #fff;
+        padding: 12px 0 20px 0;
         .u-tab_item {
             color: @fontcolor3;
             .fz(18px,28px);
@@ -216,7 +217,7 @@ export default {
         }
     }
     .u-card-title {
-        margin: 20px 0 12px 0;
+        .mb(12px);
         color: @fontcolor;
         .fz(18px,28px);
         .bold(700);
