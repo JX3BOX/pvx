13b0462fc937b61c0d9cc13db5a31034b23cadb8
TiAmo <361258177@qq.com>
2025-05-22 19:41:52 +0800

    feat：考试-小程序端

--- src/pages/exam/router.js
+++ src/pages/exam/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import { isMiniProgram } from "@jx3box/jx3box-common/js/utils";
 
 // 解决重复点击路由报错的BUG
 const originalPush = VueRouter.prototype.push;
@@ -9,7 +10,6 @@ VueRouter.prototype.push = function push(location) {
 
 Vue.use(VueRouter);
 
-const Index = () => import("@/views/exam/Index.vue");
 const Paper = () => import("@/views/exam/Paper.vue");
 const Question = () => import("@/views/exam/Question.vue");
 const QuestionPublish = () => import("@/views/exam/QuestionPublish.vue");
@@ -17,7 +17,11 @@ const PaperPublish = () => import("@/views/exam/PaperPublish.vue");
 const GameQuestionPublish = () => import("@/views/exam/GameQuestionPublish.vue");
 
 const routes = [
-    { name: "index", path: "/:type?", component: Index },
+    {
+        name: "index",
+        path: "/:type?",
+        component: isMiniProgram() ? () => import("@/views/exam/mobile/exam.vue") : () => import("@/views/exam/Index.vue"),
+    },
     { name: "paper", path: "/paper/:id?", component: Paper },
     { name: "question", path: "/question/:id?", component: Question },
     { name: "questionPublish", path: "/questionPublish/:id?", component: QuestionPublish },
---
+++ src/views/exam/mobile/exam.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="m-exam">
+        <div class="m-search">
+            <img class="u-icon" src="@/assets/img/exam/mobile/search.svg" svg-inline />
+            <input class="u-input" type="text" placeholder="输入2个字符以上的关键词搜索~" @input="searchEvent" />
+        </div>
+
+        <div class="m-exam-list">
+            <div class="m-exam-item" v-for="item in dataList" :key="'exam' + item.rid">
+                <div class="u-title">{{ item.title }}</div>
+                <div class="u-answer">{{ getAnswer(item) }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { getExamByKey } from "@/service/exam.js";
+export default {
+    components: {},
+    data() {
+        return {
+            dataList: [],
+        };
+    },
+    computed: {
+        client() {
+            return this.$store.state.client;
+        },
+    },
+    created() {},
+    methods: {
+        searchEvent(event) {
+            const searchValue = event.target.value;
+
+            if (this.searchTimer) {
+                clearTimeout(this.searchTimer);
+            }
+
+            this.searchTimer = setTimeout(() => {
+                if (searchValue.length >= 2) {
+                    getExamByKey({
+                        key: searchValue,
+                    }).then((res) => {
+                        this.dataList = res.data.data;
+                    });
+                }
+            }, 300);
+        },
+        getAnswer(item) {
+            const answers = JSON.parse(item.answer) || [];
+            const options = JSON.parse(item.options) || [];
+            return options.filter((item, index) => answers.includes(index)).join();
+        },
+    },
+};
+</script>
+<style lang="less">
+@brand4: #ffffff;
+@brand4-dark: #282828;
+@brand2: #24292e;
+@brand2-dark: #fedaa3;
+.m-exam {
+    position: relative;
+    z-index: 1;
+    &::after {
+        content: "";
+        display: block;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: #fafafa;
+        z-index: -1;
+    }
+
+    .m-search {
+        position: fixed;
+        bottom: 103px;
+        left: 12px;
+        right: 12px;
+        padding: 12px 20px;
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        background-color: #282828;
+        border-radius: 16px;
+        .u-icon {
+            .size(24px);
+        }
+        .u-input {
+            flex: 1;
+            background-color: transparent;
+            color: #fedaa3;
+            border: none;
+            font-size: 14px;
+            line-height: 20px;
+            &::placeholder {
+                color: #fedaa3;
+            }
+        }
+    }
+
+    .m-exam-list {
+        padding-top: 12px;
+        padding-bottom: 120px;
+        .m-exam-item {
+            background-color: @brand4;
+            padding: 16px;
+            border-radius: 12px;
+            border: 1px solid rgba(40, 40, 40, 0.05);
+            .u-title {
+                color: @brand2;
+                font-size: 16px;
+                font-weight: bold;
+                line-height: 24px;
+            }
+            .u-answer {
+                margin-top: 12px;
+                color: #00c7be;
+                font-size: 14px;
+                line-height: 20px;
+            }
+            &:not(:last-child) {
+                margin-bottom: 12px;
+            }
+        }
+    }
+}
+@media (prefers-color-scheme: dark) {
+    .m-exam {
+        &::after {
+            background: #000;
+        }
+
+        .m-exam-list {
+            .m-exam-item {
+                background-color: @brand4-dark;
+                .u-title {
+                    color: @brand2-dark;
+                }
+            }
+        }
+    }
+}
+</style>
