39273de3058a3e4f310b695e4368eb0f574a5afb
Kaviilee <jiaweilee95@126.com>
2026-04-04 03:45:02 +0800

    feat: 添加兼容老版本链接的逻辑

--- src/Root.vue
+++ src/Root.vue
@@ -5,5 +5,16 @@
 <script>
 export default {
     name: "Root",
+    // 兼容老版本链接，后续可以删除
+    mounted() {
+        const path = this.$route.path;
+        if (path == "/pvx/codex/adventure/treasure/landscape") {
+            window.open("/codex/adventure/landscape", "_self");
+        }
+        if (path == "/pvx/codex/adventure/treasure/portrait") {
+            window.open("/codex/adventure/portrait", "_self");
+        }
+
+    }
 };
 </script>
