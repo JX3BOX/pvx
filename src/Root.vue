83a4bc2ebbf0690dbdac07fe72e632b32cd690c8
Kaviilee <jiaweilee95@126.com>
2026-04-04 03:56:51 +0800

    feat: 更新根组件，添加图片和数据路径的提供逻辑，优化老版本链接兼容性

--- src/Root.vue
+++ src/Root.vue
@@ -3,16 +3,20 @@
 </template>
 
 <script>
+import { __imgPath, __dataPath } from "@/utils/config";
 export default {
     name: "Root",
+    provide: {
+        __imgRoot: __imgPath + "adventure/",
+        __dataRoot: __dataPath + "pvx/",
+        __imgPath,
+    },
     // 兼容老版本链接，后续可以删除
     mounted() {
-        const path = this.$route.path;
-        if (path == "/pvx/codex/adventure/treasure/landscape") {
+        const pathname = window.location.pathname;
+
+        if (pathname == "/pvx/codex/adventure/treasure") {
             window.open("/codex/adventure/landscape", "_self");
-        }
-        if (path == "/pvx/codex/adventure/treasure/portrait") {
-            window.open("/codex/adventure/portrait", "_self");
         }
 
     }
