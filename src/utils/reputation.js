0818ca123d47e1f4b1d3ed8d8deb51e67cad8783
fifthThirteen <570205219@qq.com>
2026-06-06 13:59:32 +0800

    fix:声望默认图片

--- src/utils/reputation.js
+++ src/utils/reputation.js
@@ -1,29 +1,30 @@
 import { __imgPath } from "@/utils/config";
 import paths from "@/assets/data/reputation_exchange_path.json";
 import levelList from "@/assets/data/reputation_level.json";
+import defaultReputationIcon from "@/assets/img/reputation/reputation-null.png";
 
 export const LOST_RESPECT_UNAVAILABLE = "无法使用遗失的尊敬来提高该声望等级进度";
+export const DEFAULT_REPUTATION_ICON = defaultReputationIcon;
+
+function getIconName(iconPath) {
+    const rPath = iconPath ? iconPath.replace(/\//g, "\\") : "";
+    return rPath ? rPath.split("\\")[rPath.split("\\").length - 1].toLowerCase().split(".tga")[0] : "";
+}
 
 export function getReputationIcon(iconPath, type = "icon") {
-    const rPath = iconPath ? iconPath.replace(/\//g, "\\") : "";
-    const iconName = rPath
-        ? rPath.split("\\")[rPath.split("\\").length - 1].toLowerCase().split(".tga")[0]
-        : "";
+    const iconName = getIconName(iconPath);
     if (iconName) {
         return __imgPath + "reputation/reputation/std/" + type + "/" + iconName + ".png";
     }
-    return "";
+    return DEFAULT_REPUTATION_ICON;
 }
 
 export function getBotIcon(iconPath) {
-    const rPath = iconPath ? iconPath.replace(/\//g, "\\") : "";
-    const iconName = rPath
-        ? rPath.split("\\")[rPath.split("\\").length - 1].toLowerCase().split(".tga")[0]
-        : "";
+    const iconName = getIconName(iconPath);
     if (iconName) {
         return __imgPath + "reputation/reputation/std/icon/" + iconName + ".png";
     }
-    return "";
+    return DEFAULT_REPUTATION_ICON;
 }
 
 export function getReputationPath(name) {
